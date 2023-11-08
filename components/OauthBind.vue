25513675452c0a9a7bb63a08406f6977ac061c14
truthhun <1272881215@qq.com>
2023-11-08 12:33:06 +0800

    调整前端

--- components/OauthBind.vue
+++ components/OauthBind.vue
@@ -3,62 +3,10 @@
         <div class="avatar text-center">
             <el-avatar :size="64" :src="oauth.avatar"></el-avatar>
         </div>
-        <el-form :model="bindData" label-width="80px" ref="oauth"  label-position="top">
-            <div v-if="existAccount">
-                    <el-form-item label="登录账号" prop="username" :rules="[{
-                        required: true,
-                        message: '请输入用户名、邮箱或手机号码',
-                        trigger: 'blur'
-                    }]">
-                        <el-input v-model="bindData.username" placeholder="请输入用户名、邮箱或手机号码"></el-input>
-                    </el-form-item>
-                    <el-form-item label="登录密码" prop="password" :rules="[{
-                        required: true,
-                        message: '请输入密码',
-                        trigger: 'blur'
-                    }]">
-                        <el-input v-model="bindData.password" type="password" placeholder="请输入密码"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="bindOauth" class="btn-block" icon="el-icon-connection">登录并绑定</el-button>
-                    </el-form-item>
-                <div class="text-center">已有帐号？<el-button type="text" @click="switchBind">点此绑定</el-button></div>
-            </div>
-            <div v-else>
-                    <el-form-item label="邮箱" prop="email" :rules="[{
-                        required: true,
-                        message: '请输入邮箱，以便用于找回密码',
-                        trigger: 'blur'
-                    }]">
-                        <el-input v-model="bindData.email" placeholder="请输入邮箱，以便用于找回密码"></el-input>
-                    </el-form-item>
-                    <el-form-item label="用户名" prop="username" :rules="[{
-                        required: true,
-                        message: '请输入用户名',
-                        trigger: 'blur'
-                    }]">
-                        <el-input v-model="bindData.username" placeholder="请输入用户名"></el-input>
-                    </el-form-item>
-                    <el-form-item label="登录密码" prop="password" :rules="[{
-                        required: true,
-                        message: '请输入密码',
-                        trigger: 'blur'
-                    }]">
-                        <el-input v-model="bindData.password" type="password" placeholder="请输入密码"></el-input>
-                    </el-form-item>
-                    <el-form-item label="确认密码" prop="repeat_password" :rules="[{
-                        required: true,
-                        message: '请再次输入密码',
-                        trigger: 'blur'
-                    }]">
-                        <el-input v-model="bindData.repeat_password" type="password" placeholder="请再次输入密码"></el-input>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="bindOauth" class="btn-block" icon="el-icon-connection">注册并绑定</el-button>
-                    </el-form-item>
-                <div class="text-center">没有帐号？<el-button type="text" @click="switchBind">注册一个</el-button> </div>
-            </div>
-        </el-form>
+        <FormLogin v-if="existAccount"/>
+        <FormRegister v-else />
+        <div class="text-center" v-if="existAccount">没有帐号？<el-button type="text" @click="switchBind">注册一个</el-button> </div>
+        <div class="text-center" v-else>已有帐号？<el-button type="text" @click="switchBind">点此绑定</el-button></div>
     </div>
 </template>
 <script>
