4f34eb0631f84bbbbb9fe2f7bdae41b1ebccbf60
truthhun <1272881215@qq.com>
2025-02-18 22:11:30 +0800

    添加微信分享功能，更新分享数据方法以支持最新的分享接口

--- components/WxShare.vue
+++ components/WxShare.vue
@@ -25,6 +25,8 @@ export default {
           'getLocation',
           'onMenuShareTimeline',
           'onMenuShareAppMessage',
+          'updateAppMessageShareData',
+          'updateTimelineShareData',
         ],
       },
       link: location.href,
