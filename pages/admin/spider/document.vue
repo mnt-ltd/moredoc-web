0180e8378ccfade2dc280c8829913d745cde91f3
truthhun <1272881215@qq.com>
2025-04-27 11:53:35 +0800

    清理 batchUpdateSpiderDocument 方法中的多余字段，优化数据处理

--- pages/admin/spider/document.vue
+++ pages/admin/spider/document.vue
@@ -404,6 +404,9 @@ export default {
           delete newItem.url_html
           delete newItem.editing
           delete newItem.disable_delete
+          delete newItem._X_ROW_CHILD
+          delete newItem._X_ROW_KEY
+          delete newItem.children
           return newItem
         })
         const res = await batchUpdateSpiderDocument({ spider_document: docs })
