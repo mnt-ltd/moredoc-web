bbc2ff513b563ae0b6cc5b2843f6e418ede2080a
truthhun <1272881215@qq.com>
2025-08-28 17:33:51 +0800

    更新页面标题，移除默认值以确保一致性

--- pages/index.vue
+++ pages/index.vue
@@ -442,7 +442,7 @@ export default {
   },
   head() {
     return {
-      title: this.settings.system.title || 'MOREDOC · 魔豆文库',
+      title: this.settings.system.title,
       meta: [
         {
           hid: 'keywords',
