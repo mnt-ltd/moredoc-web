17d9b9537fc377f55282ec3580a7d60b67348d31
truthhun <1272881215@qq.com>
2026-01-21 10:59:07 +0800

    添加用户身份验证逻辑，确保用户存在且具有有效 ID 后才能发布文章和上传文档

--- pages/post.vue
+++ pages/post.vue
@@ -127,6 +127,10 @@ export default {
     ...mapActions('category', ['getCategories']),
     ...mapActions('user', ['getUserGroups']),
     canIPublishArticle() {
+      if (!this.user || !this.user.id) {
+        this.canIPublish = false
+        return
+      }
       this.groups.forEach((group) => {
         if (group.enable_article) {
           this.canIPublish = true
