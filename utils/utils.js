9e6ca7e9a8de4fcc33f5b4bf76e00cb1a3f0cfab
truthhun <1272881215@qq.com>
2026-01-29 21:10:00 +0800

    修复百度统计失效的问题

--- utils/utils.js
+++ utils/utils.js
@@ -301,5 +301,9 @@ export function extractScripts(scriptContent) {
     scripts.push(match[1])
   }
 
+  if (scripts.length === 0 && scriptContent.trim() !== '') {
+    scripts.push(scriptContent)
+  }
+
   return scripts
 }
