c3233103ee89b8f19a1d837492a0b89679e56354
进击的皇虫 <truthhun@qq.com>
2026-03-10 09:29:29 +0800

    优化首页，移除通知板组件的多余卡片包装

--- pages/index.vue
+++ pages/index.vue
@@ -62,9 +62,7 @@
       <div>
         <el-row :gutter="20">
           <el-col :span="24">
-            <el-card shadow="never">
-              <notice-board :notices="notices" />
-            </el-card>
+            <notice-board :notices="notices" />
           </el-col>
         </el-row>
       </div>
