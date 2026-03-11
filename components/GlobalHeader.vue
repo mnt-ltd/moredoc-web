f3a4c33e73bc7ee408bbfb1f30cd6aa9d30cf6b3
进击的皇虫 <truthhun@qq.com>
2026-03-11 21:27:49 +0800

    优化搜索模态框聚焦延迟时间，调整横向菜单样式，提高界面一致性

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -575,7 +575,7 @@ export default {
       this.$nextTick(() => {
         setTimeout(() => {
           this.$refs.searchModalInput.focus()
-        }, 500)
+        }, 300)
       })
     },
     closeSearchModal() {
@@ -726,6 +726,11 @@ export default {
     }
     .el-menu--horizontal > .el-submenu .el-submenu__title {
       padding-top: 1px;
+      height: 70px;
+      line-height: 70px;
+      font-size: 15px;
+      font-weight: 500 !important;
+      color: unset;
     }
     .el-menu.el-menu--horizontal {
       border-bottom: 0;
